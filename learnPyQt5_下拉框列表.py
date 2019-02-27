c200748c538f5dcc28e61a01019ae22c557aa87d
TechDP_MegaHunt <1095995709@qq.com>
2019-02-27 20:03:57 +0800

    新增下拉选择框学习代码

---
+++ learnPyQt5_下拉框列表.py
@@ -0,0 +1,17 @@
+#!usr/bin/python3
+#-*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QComboBox, QLabel
+
+class Example(QWidget):
+    def __init__():
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    
